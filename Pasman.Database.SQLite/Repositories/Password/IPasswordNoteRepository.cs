ede393bb38bb1df033a150a349bd8ff51a758aa2
artemkiruhin <temkaunik@gmail.com>
2024-11-22 21:51:33 +0300

    Password note repository interface

---
+++ Pasman.Database.SQLite/Repositories/Password/IPasswordNoteRepository.cs
@@ -0,0 +1,9 @@
+﻿using Pasman.Database.SQLite.Repositories.Base;
+using Pasman.Domain.Entities;
+
+namespace Pasman.Database.SQLite.Repositories.Password;
+
+public interface IPasswordNoteRepository : ICrudRepository<PasswordNote>
+{
+    
+}
