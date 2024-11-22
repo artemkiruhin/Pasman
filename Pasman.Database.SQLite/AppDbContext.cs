cb5a585345013fb21395bd5464e78c4ec4b7c423
artemkiruhin <temkaunik@gmail.com>
2024-11-22 21:35:19 +0300

    Database context for sqlite

---
+++ Pasman.Database.SQLite/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pasman.Domain.Entities;
+
+namespace Pasman.Database.SQLite;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<AccountData> AccountData { get; set; } = null!;
+    public DbSet<PasswordNote> PasswordNotes { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=passwords.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<AccountData>(options =>
+        {
+            options.HasNoKey();
+            options.Property(x => x.KeyHash).IsRequired();
+        });
+        
+        
+        modelBuilder.Entity<PasswordNote>(options =>
+        {
+            options.HasKey(x => x.Id);
+            options.Property(x => x.Description).IsRequired();
+            options.Property(x => x.PasswordEncrypted).IsRequired();
+        });
+    }
+}
