473356bb27a81cf3cc166707c0689608472cd785
artemkiruhin <temkaunik@gmail.com>
2024-11-22 21:51:00 +0300

    Base repository interface

---
+++ Pasman.Database.SQLite/Repositories/Base/ICrudRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Pasman.Database.SQLite.Repositories.Base;
+
+public interface ICrudRepository<TEntity>
+{
+    Task<bool> AddAsync(TEntity entity);
+    Task<bool> UpdateAsync(TEntity entity);
+    Task<bool> DeleteAsync(Guid id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<TEntity?> GetByIdAsync(Guid id);
+}
